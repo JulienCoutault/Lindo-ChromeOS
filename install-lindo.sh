07b482d0ba569e18c72752403b0f0a92235621c5
Programmateur01 <git@juliencoutault.fr>
2020-03-31 00:29:03 +0200

    Update install-lindo.sh

--- install-lindo.sh
+++ install-lindo.sh
@@ -3,6 +3,7 @@
 # Download lindo 2.4.0
 wget https://github.com/prixe/lindo/releases/download/v2.4.0/lindo-2.4.0.tar.gz
 tar -xf lindo-2.4.0.tar.gz
+rm -v lindo-2.4.0.tar.gz
 
 # Install require libs
 sudo apt-get install libssl1.1.0 libssl-dev -y
