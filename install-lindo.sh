fed4f67eac509dc992b6dce8e9ada41de52807d2
Programmateur01 <git@juliencoutault.fr>
2020-10-07 12:50:23 +0200

    Update install-lindo.sh

--- install-lindo.sh
+++ install-lindo.sh
@@ -6,8 +6,10 @@ tar -xf lindo-2.4.0.tar.gz
 rm -v lindo-2.4.0.tar.gz
 
 # Install require libs
-sudo apt-get install libssl1.1.0 libssl-dev -y
-sudo ln -s /lib/x86_64-linux-gnu/libssl.so /usr/lib/libssl.so.10
 sudo apt install libnss3 -y
 
 ln -s lindo-2.4.0/Lindo lindo
+
+echo ""
+echo "--------------------------------------------------"
+echo "To play write : ./lindo"
