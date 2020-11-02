621843eafe36c89e8b960891b8bb78c3f3397740
Programmateur01 <git@juliencoutault.fr>
2020-11-02 18:56:10 +0100

    v2.5.1

--- install-lindo.sh
+++ install-lindo.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Download lindo 2.4.0
-wget https://github.com/prixe/lindo/releases/download/v2.4.0/lindo-2.4.0.tar.gz
-tar -xf lindo-2.4.0.tar.gz
-rm -v lindo-2.4.0.tar.gz
+# Download lindo 2.5.1
+wget https://github.com/prixe/lindo/releases/download/v2.5.1/lindo-2.5.1.tar.gz
+tar -xf lindo-2.5.1.tar.gz
+rm -v lindo-2.5.1.tar.gz
 
 # Install require libs
 sudo apt install libnss3 -y
 
-ln -s lindo-2.4.0/Lindo lindo
+ln -s lindo-2.5.1/Lindo lindo
 
 echo ""
 echo "--------------------------------------------------"
