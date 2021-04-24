eb9926dbabc7a6973cbf7715c5f687377c4afbbb
Programmateur01 <git@juliencoutault.fr>
2021-04-24 17:09:59 +0200

    Can now install lindo beta

--- install-lindo.sh
+++ install-lindo.sh
@@ -1,19 +1,44 @@
 #!/bin/bash
 
-# Download lindo 2.5.4
-wget https://github.com/prixe/lindo/releases/download/v2.5.4/lindo-2.5.4.tar.gz
-tar -xf lindo-2.5.4.tar.gz
-rm -v lindo-2.5.4.tar.gz
+version='2.5.4'
+
+if [ -z $1 ]; then
+    $1 = 'a'
+fi
+
+if [ $1 == 'beta' ]; then
+    version='2.6.0-beta.11'
+fi
+
+echo "Downloading lindo ${version}"
+
+# Download lindo
+wget https://github.com/prixe/lindo/releases/download/v${version}/lindo-${version}.tar.gz
+tar -xf lindo-${version}.tar.gz
+rm -v lindo-${version}.tar.gz
 
 # Install require libs
 sudo apt install libnss3 -y
 
-if [ -f "lindo" ]; then
-    # shotcut already exist
-    rm lindo
+if [ $1 == 'beta' ]; then
+    if [ -f "lindo-beta" ]; then
+        # shotcut already exist
+        rm lindo-beta
+    fi
+    ln -s lindo-${version}/Lindo lindo-beta
+else
+    if [ -f "lindo" ]; then
+        # shotcut already exist
+        rm lindo
+    fi
+    ln -s lindo-${version}/Lindo lindo
 fi
-ln -s lindo-2.5.4/Lindo lindo
 
 echo ""
 echo "--------------------------------------------------"
-echo "To play write : ./lindo"
+echo "Lindo ${version} was downloaded"
+if [ $1 == 'beta' ]; then
+    echo "To play write : ./lindo-beta"
+else
+    echo "To play write : ./lindo"
+fi
